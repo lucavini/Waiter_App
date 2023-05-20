1ce5ecc451d02709b7b0a6c505ec7b57555c06ff
lucavini <lucass.lv.lv@gmail.com>
2023-05-20 20:34:37 -0300

    feat: mongodb connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3333;
 
-app.listen(3333, () => {
-  console.log('Server is running on http://localhost:3333');
-});
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('mongo connection refused'));
