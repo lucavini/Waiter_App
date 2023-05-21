334305b7d6628ec6c91b76133782a2cc27d3c464
lucavini <lucass.lv.lv@gmail.com>
2023-05-20 21:10:26 -0300

    feat: created models

---
+++ backend/src/App/models/Order.ts
@@ -0,0 +1,35 @@
+import { model, Schema } from 'mongoose';
+
+export const Order = model('Order', new Schema({
+  table: {
+    type: String,
+    required: true,
+  },
+
+  status: {
+    type: String,
+    enum: ['WAITING', 'IN_PRODUCTION', 'DONE'],
+    default: 'WAITING',
+    required: true,
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+
+    products: {
+      required: true,
+      type: [{
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+          required: true
+        },
+      }]
+    }
+  }
+}));
---
+++ backend/src/App/models/Product.ts
@@ -0,0 +1,38 @@
+import { model, Schema } from 'mongoose';
+
+export const Product = model('Product',new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  imagePath: {
+    type: String,
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+
+  ingredients: {
+    required: true,
+    type: [{
+      name: String,
+      icon: String,
+    }],
+  },
+
+  category: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Category',
+  },
+}),
+);
