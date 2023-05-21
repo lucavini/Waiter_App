9e2256ce9c0896e258b07bcf820aab4e853af037
lucavini <lucass.lv.lv@gmail.com>
2023-05-20 23:16:53 -0300

    feat: use router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { router } from './App/routes/router';
 
-mongoose.connect('mongodb://localhost:27017')
+mongoose
+  .connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
     const port = 3333;
 
+    app.use(router);
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
