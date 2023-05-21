1472b6ee799a7f5acc0f9326865213ed22925d1f
lucavini <lucass.lv.lv@gmail.com>
2023-05-21 12:32:20 -0300

    feat: use express json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { router } from './App/routes/router';
 
-mongoose
-  .connect('mongodb://localhost:27017')
+mongoose.connect('mongodb://localhost:27017/WaiterApp')
   .then(() => {
     const app = express();
     const port = 3333;
 
+    app.use(express.json());
     app.use(router);
+
     app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
+      console.log(`🚀 Server is running on http://localhost:${port}`);
     });
   })
   .catch(() => console.log('mongo connection refused'));
