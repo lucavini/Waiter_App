37c12cf465898dec31dd1fed2ebbe045b739f17e
lucavini <lucass.lv.lv@gmail.com>
2024-01-09 18:45:17 -0300

    feat: add entities models

--- web/src/Shared/Models/models.d.ts
+++ web/src/Shared/Models/models.d.ts
@@ -1,9 +1,29 @@
 export as namespace models;
 
 export interface Category {
-  categories: {
-    idCategory: string;
-    strCategory: string;
-    strCategoryThumb: string;
+  _id: string;
+  name: string;
+  icon: string;
+}
+
+export type Ingredients = Category 
+
+export interface Product {
+  _id: string;
+  name: string;
+  description: string;
+  imagePath: string;
+  price: number;
+  ingredients: Ingredients[];
+  category: string;
+}
+
+export interface Order {
+  _id: string;
+  table: string;
+  status: string;
+  products: {
+    product: Product;
+    quantity: number;
   }[];
 }
